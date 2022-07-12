0608cca9c35710930fdf7e53b7a0ddb8ae964eb7
Sam <sam135642@gmail.com>
2022-07-12 20:41:50 +0800

    feat : Add data table & delete function with cache update

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Header from "./component/Header"
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Client from "./component/Client"
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge (existing, incoming) {
+            return incoming
+          }
+        },
+        projects: {
+          merge (existing, incoming) {
+            return incoming
+          }
+        }
+      }
+    }
+  }
+})
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
+  cache
 })
 
 function App () {
--- client/src/component/Client.jsx
+++ client/src/component/Client.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
 import { gql, useQuery } from '@apollo/client'
 //gql : make a query
-
-const GET_CLIENTS = gql`
-  query getClients {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`
+import ClientRow from './ClientRow'
+import { GET_CLIENTS } from '../queries/clientQueries'
+import Spinner from './Spinner'
 
 export default function Client() {
   const { loading, error, data } = useQuery(GET_CLIENTS)
 
-  if (loading) return <p>Loading...</p>
+  if (loading)
+    return (
+      <p>
+        <Spinner />
+      </p>
+    )
   if (error) return <p>Something Went Wrong</p>
 
-  return <>{!loading && !error && <h1>Clients</h1>}</>
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  )
 }
