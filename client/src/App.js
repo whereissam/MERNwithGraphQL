85f0d648799129b45124af0d8b63519deb840796
Sam <63341022+whereissam@users.noreply.github.com>
2023-01-03 17:03:43 +0800

    Add Project mutation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import Header from "./component/Header"
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Header from "./components/Header"
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import Client from "./component/Client"
-import Projects from "./component/Projects."
-import AddClientModal from "./component/AddClientModal"
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Project from './pages/Project'
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -32,12 +35,17 @@ function App () {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header></Header>
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Client></Client>
-        </div>
+        <Router>
+          <Header></Header>
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   )
